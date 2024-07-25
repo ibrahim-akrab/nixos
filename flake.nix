b4d60388f44c0a74f091eaf14c1313c63aa1cd10
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2024-07-25 22:16:33 +0300

    add my user home-manager configuration

--- flake.nix
+++ flake.nix
@@ -95,8 +95,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
+      "ibrahim" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
