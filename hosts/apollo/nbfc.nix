969c539a8a5383f43b488b9158e62e4d9f80eb42
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2024-07-31 02:23:42 +0300

    comment out nbfc service since I only care about ec_probe binary

--- hosts/apollo/nbfc.nix
+++ hosts/apollo/nbfc.nix
@@ -12,12 +12,12 @@ in {
   environment.systemPackages = with pkgs; [
     inputs.nbfc-linux.packages.x86_64-linux.default
   ];
-  systemd.services.nbfc_service = {
-    enable = true;
-    description = "NoteBook FanControl service";
-    serviceConfig.Type = "simple";
-    path = [pkgs.kmod];
-    script = "${inputs.nbfc-linux.packages.x86_64-linux.default}/${command}";
-    wantedBy = ["multi-user.target"];
-  };
+#   systemd.services.nbfc_service = {
+#     enable = true;
+#     description = "NoteBook FanControl service";
+#     serviceConfig.Type = "simple";
+#     path = [pkgs.kmod];
+#     script = "${inputs.nbfc-linux.packages.x86_64-linux.default}/${command}";
+#     wantedBy = ["multi-user.target"];
+#   };
 }
