f5a171e0e12e22ebf4981624ddc8d5bc03cd045e
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2024-08-02 20:02:46 +0300

    embed fs-diff script into system packages

--- hosts/apollo/default.nix
+++ hosts/apollo/default.nix
@@ -157,6 +157,38 @@ in
       sudo mkdir -p "/persist/$(dirname $1)"
       sudo cp -r {,/persist}$1
     '')
+    (writeShellScriptBin "fs-diff" ''
+      cleanup() {
+        sudo umount /btrfs_tmp
+        sudo rmdir /btrfs_tmp
+      }
+
+      trap cleanup EXIT
+
+      sudo mkdir /btrfs_tmp
+      sudo mount -o subvol=/ /dev/mapper/crypted /btrfs_tmp
+
+      set -euo pipefail
+
+      OLD_TRANSID=$(sudo btrfs subvolume find-new /btrfs_tmp/root-blank 9999999)
+      OLD_TRANSID=$(cut -d' ' -f4- <<< "$OLD_TRANSID")
+
+      sudo btrfs subvolume find-new "/btrfs_tmp/root" "$OLD_TRANSID" |
+      sed '$d' |
+      cut -f17- -d' ' |
+      sort |
+      uniq |
+      while read path; do
+        path="/$path"
+        if [ -L "$path" ]; then
+          : # The path is a symbolic link, so is probably handled by NixOS already
+        elif [ -d "$path" ]; then
+          : # The path is a directory, ignore
+        else
+          echo "$path"
+        fi
+      done
+    '')
   ];
   environment.persist."/persist" = {
     hideMounts = true;
@@ -164,6 +196,7 @@ in
       "/etc/NetworkManager/system-connections"
       "/var/lib/NetworkManager"
       "/var/lib/nixos"
+      "/var/lib/fprint"
     ];
     files = [
       "/etc/machine-id"
