e0e35c40bab4ff62734440920d40cd1f843bd19d
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2024-08-02 18:37:43 +0300

    re-enable impermenance with new script

--- hosts/apollo/default.nix
+++ hosts/apollo/default.nix
@@ -26,47 +26,34 @@ in
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # delete root on boot
-  # boot.initrd = {
-  #   enable = true;
-  #   supportedFilesystems = [ "btrfs" ];
-  #   postResumeCommands = lib.mkAfter ''
-  #     mkdir -p /mnt
-  #     # We first mount the btrfs root to /mnt
-  #     # so we can manipulate btrfs subvolumes.
-  #     mount -o subvol=/ /dev/mapper/crypted /mnt
+  boot.initrd = {
+    enable = true;
+    supportedFilesystems = [ "btrfs" ];
+    postResumeCommands = lib.mkAfter ''
+      mkdir /btrfs_tmp
+      mount -o subvol=root /dev/mapper/crypted /btrfs_tmp
+      if [[ -e /btrfs_tmp/root ]]; then
+          mkdir -p /btrfs_tmp/old_roots
+          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+          mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+      fi
 
-  #     # While we're tempted to just delete /root and create
-  #     # a new snapshot from /root-blank, /root is already
-  #     # populated at this point with a number of subvolumes,
-  #     # which makes `btrfs subvolume delete` fail.
-  #     # So, we remove them first.
-  #     #
-  #     # /root contains subvolumes:
-  #     # - /root/var/lib/portables
-  #     # - /root/var/lib/machines
-  #     #
-  #     # I suspect these are related to systemd-nspawn, but
-  #     # since I don't use it I'm not 100% sure.
-  #     # Anyhow, deleting these subvolumes hasn't resulted
-  #     # in any issues so far, except for fairly
-  #     # benign-looking errors from systemd-tmpfiles.
-  #     btrfs subvolume list -o /mnt/root |
-  #     cut -f9 -d' ' |
-  #     while read subvolume; do
-  #       echo "deleting /$subvolume subvolume..."
-  #       btrfs subvolume delete "/mnt/$subvolume"
-  #     done &&
-  #     echo "deleting /root subvolume..." &&
-  #     btrfs subvolume delete /mnt/root
+      delete_subvolume_recursively() {
+          IFS=$'\n'
+          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+              delete_subvolume_recursively "/btrfs_tmp/$i"
+          done
+          btrfs subvolume delete "$1"
+      }
 
-  #     echo "restoring blank /root subvolume..."
-  #     btrfs subvolume snapshot /mnt/root-blank /mnt/root
+      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+          delete_subvolume_recursively "$i"
+      done
 
-  #     # Once we're done rolling back to a blank snapshot,
-  #     # we can unmount /mnt and continue on the boot process.
-  #     umount /mnt
-  #   '';
-  # };
+      btrfs subvolume create /btrfs_tmp/root
+      umount /btrfs_tmp
+    '';
+  };
 
 
   networking.hostName = "apollo"; # Define your hostname.
