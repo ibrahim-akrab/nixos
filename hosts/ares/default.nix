4e0c3f51850a3af5cfc8335c65a2922f433d91df
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-03-24 13:09:16 +0200

    move to using wayland instead of xserver for ares

--- hosts/ares/default.nix
+++ hosts/ares/default.nix
@@ -65,14 +65,17 @@
 
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  # services.xserver.xkb = {
+    # layout = "us";
+    # variant = "";
+  # };
 
   # Enable bluetooth
   hardware.bluetooth.enable = true; # enables support for Bluetooth
