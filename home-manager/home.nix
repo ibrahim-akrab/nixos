2a52bb3e01ff70a48d0354055f4d61ab78a01547
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-06-26 19:15:34 +0300

    conditionally enable vscode in home-manager only in nixos-wsl (for real this time)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -52,7 +52,7 @@
   ];
 
   programs.vscode = {
-    enable = !osConfig.wsl.enable or true;	# disable it if inside wsl (since it falls back to windows native version)
+    enable = !(osConfig.wsl.enable or false);	# disable it if inside wsl (since it falls back to windows native version)
     profiles.default.extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       vscodevim.vim
