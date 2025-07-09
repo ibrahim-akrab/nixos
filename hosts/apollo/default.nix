ee0a27bfeb276cf89e84dd5f84674a943550e543
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-07-10 02:46:50 +0800

    enable mac masking to avoid fingerprinting

--- hosts/apollo/default.nix
+++ hosts/apollo/default.nix
@@ -99,7 +99,11 @@ in {
   networking.hostName = "apollo"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  networking.networkmanager = { # Easiest to use and most distros use this by default.
+    enable = true;
+    wifi.macAddress = "stable-ssid";
+    ethernet.macAddress = "stable";
+  };
 
   # Set your time zone.
   time.timeZone = "Asia/Manila";
