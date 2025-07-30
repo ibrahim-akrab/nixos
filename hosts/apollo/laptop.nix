27a75ed575e7fdbbac5dc723a3ab103016b8ad1f
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-07-31 05:52:09 +0800

    disable system76 scheduler

--- hosts/apollo/laptop.nix
+++ hosts/apollo/laptop.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   # Better scheduling for CPU cycles - thanks System76!!!
-  services.system76-scheduler.settings.cfsProfiles.enable = true;
+  # services.system76-scheduler.settings.cfsProfiles.enable = true;
 
   # Enable TLP (better than gnomes internal power manager)
   services.tlp = {
