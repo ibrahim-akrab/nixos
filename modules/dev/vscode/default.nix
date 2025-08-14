86ed7fc9afc523f83965eddae68315a7666a284f
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-08-14 22:01:01 +0800

    add vscode configuration settings to home-manager module

---
+++ modules/dev/vscode/default.nix
@@ -0,0 +1,26 @@
+{
+  flake.modules.homeManager.dev =
+    {
+      pkgs,
+      osConfig,
+      ...
+    }:
+    {
+      programs.vscode = {
+        enable = !(osConfig.wsl.enable or false); # disable it if inside wsl (since it falls back to windows native version)
+        profiles.default = {
+          extensions = with pkgs.vscode-extensions; [
+            dracula-theme.theme-dracula
+            vscodevim.vim
+            yzhang.markdown-all-in-one
+            ms-python.python
+            ms-pyright.pyright
+            kamadorueda.alejandra
+            bbenoist.nix
+          ];
+          userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
+          keybindings = builtins.fromJSON (builtins.readFile ./keybindings.json);
+        };
+      };
+    };
+}
