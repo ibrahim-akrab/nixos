0922106e0ef34778dba40c2075bd1d6147651fc6
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-08-16 12:11:54 +0800

    re-enable caps-lock as it is

--- modules/hyprland/hyprland/input.nix
+++ modules/hyprland/hyprland/input.nix
@@ -12,7 +12,7 @@
           kb_layout = "us";
           # kb_variant =
           # kb_model =
-          kb_options = "compose:caps";
+          # kb_options = "compose:caps";
           # kb_rules =
 
           follow_mouse = 1;
