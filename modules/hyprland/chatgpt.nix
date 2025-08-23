a486d2f3124b3931cfe3697b0bda747ef8988307
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-08-23 17:42:37 +0800

    change chatgpt package used

--- modules/hyprland/chatgpt.nix
+++ modules/hyprland/chatgpt.nix
@@ -7,10 +7,10 @@
       offset = (100 - size) / 2;
     in
     {
-      home.packages = [ pkgs.gpt-cli ];
+      home.packages = [ pkgs.chatgpt-cli ];
       wayland.windowManager.hyprland.settings = {
         exec-once = [
-          "[workspace special:chatgpt silent; float; move ${toString offset}% ${toString offset}%; size ${toString size}% ${toString size}%] $terminal -T ${specialTitle} --hold sh -c 'while true; do gpt --model gpt-4.1 --no_price dev; clear; done'"
+          "[workspace special:chatgpt silent; float; move ${toString offset}% ${toString offset}%; size ${toString size}% ${toString size}%] $terminal -T ${specialTitle} --hold sh -c 'while true; do chatgpt; clear; done'"
         ];
         windowrule = [ "workspace special:chatgpt, title:${specialTitle}" ];
         bind = [
