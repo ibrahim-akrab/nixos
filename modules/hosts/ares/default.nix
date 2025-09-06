b3f28c95b2a0a01114132a4dc7238ac1e2de679b
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-09-06 20:58:19 +0800

    port ares host to flake-parts

---
+++ modules/hosts/ares/default.nix
@@ -0,0 +1,35 @@
+{ config, ... }:
+let
+  my_modules = [
+    # Modules
+    "audio"
+    "base"
+    "bluetooth"
+    "desktop"
+    "dev"
+    "hyprland"
+    "mixrank"
+
+    # Users
+    "ibrahim"
+    "root"
+
+    # Host
+    "ares"
+  ];
+  nixosModulesNames = builtins.filter (name: config.flake.modules.nixos ? "${name}") my_modules;
+  homeManagerModulesNames = builtins.filter (
+    name: config.flake.modules.homeManager ? "${name}"
+  ) my_modules;
+  nixosModules = map (name: config.flake.modules.nixos."${name}") nixosModulesNames;
+  homeManagerModules = map (name: config.flake.modules.homeManager."${name}") homeManagerModulesNames;
+
+  flake.modules.nixos."hosts/ares".imports = nixosModules ++ [
+    {
+      home-manager.users.ibrahim.imports = homeManagerModules;
+    }
+  ];
+in
+{
+  inherit flake;
+}
---
+++ modules/hosts/ares/stateversion.nix
@@ -0,0 +1,5 @@
+{
+  flake.modules.nixos.ares = {
+    system.stateVersion = "23.11";
+  };
+}
