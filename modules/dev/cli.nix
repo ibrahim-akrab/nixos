2099bf3d96b2213c54ea806d54afbf45dfa55ee8
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-09-27 05:40:08 +0800

    add file command to cli

--- modules/dev/cli.nix
+++ modules/dev/cli.nix
@@ -1,35 +1,19 @@
 {
-  flake.modules.homeManager.dev = {
-    # Commandline Tools
-    programs = {
+  flake.modules.homeManager.dev =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      home.packages = with pkgs; [
+        slack
+        telegram-desktop
 
-      htop.enable = true;
-      btop.enable = true;
-      bat.enable = true;
-      bash.enable = true;
-      jq.enable = true;
-      jqp.enable = true;
-      ripgrep.enable = true;
-      fd.enable = true;
-      lazygit.enable = true;
-      yazi = {
-        enable = true;
-        enableBashIntegration = true;
-      };
-      lsd = {
-        enable = true;
-        enableBashIntegration = true;
-      };
-      zoxide = {
-        enable = true;
-        enableBashIntegration = true;
-        options = [ "--cmd cd" ];
-      };
-      direnv = {
-        enable = true;
-        enableBashIntegration = true;
-        silent = true;
-      };
+        tree
+        cht-sh
+        file
+        wget
+        git
+      ];
     };
-  };
 }
