5a47f9a8461174f5e0a29b155bdee0e6504aa124
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-09-29 18:14:21 +0800

    improve fs-diff-btrfs script

--- modules/packages/impermanence-helpers.nix
+++ modules/packages/impermanence-helpers.nix
@@ -8,36 +8,39 @@
         sudo cp -ra $@ $dir
       '';
       fs-diff-btrfs = pkgs.writeShellScriptBin "fs-diff-btrfs" ''
-        cleanup() {
-          sudo umount /btrfs_tmp
-          sudo rmdir /btrfs_tmp
-        }
+                cleanup() {
+                  sudo umount /btrfs_tmp
+                  sudo rmdir /btrfs_tmp
+                }
 
-        trap cleanup EXIT
+                trap cleanup EXIT
 
-        sudo mkdir /btrfs_tmp
-        sudo mount -o subvol=/ /dev/mapper/crypted /btrfs_tmp
+                sudo mkdir /btrfs_tmp
+                sudo mount -o subvol=/ /dev/mapper/crypted /btrfs_tmp
 
-        set -euo pipefail
+                set -euo pipefail
 
-        OLD_TRANSID=$(sudo btrfs subvolume find-new /btrfs_tmp/root_blank 9999999)
-        OLD_TRANSID=$(cut -d' ' -f4- <<< "$OLD_TRANSID")
+                OLD_TRANSID=$(sudo btrfs subvolume find-new /btrfs_tmp/root_blank 9999999)
+                OLD_TRANSID=$(cut -d' ' -f4- <<< "$OLD_TRANSID")
 
-        sudo btrfs subvolume find-new "/btrfs_tmp/root" "$OLD_TRANSID" |
-        sed '$d' |
-        cut -f17- -d' ' |
-        sort |
-        uniq |
-        while read path; do
-          path="/$path"
-          if [ -L "$path" ]; then
-            : # The path is a symbolic link, so is probably handled by NixOS already
-          elif [ -d "$path" ]; then
-            : # The path is a directory, ignore
-          else
-            echo "$path"
-          fi
-        done
+                filter_paths() {
+                  sed '$d' |
+                  cut -f17- -d' ' |
+                  sort |
+                  uniq |
+                  while read path; do
+                    path="/$path"
+                    if [ -L "$path" ]; then
+                      : # The path is a symbolic link, so is probably handled by NixOS already
+                    elif [ -d "$path" ]; then
+                      : # The path is a directory, ignore
+                    else
+                      echo "$path"
+                    fi
+                  done
+                }
+
+                sudo btrfs subvolume find-new "/btrfs_tmp/root" "$OLD_TRANSID" | filter_paths | ${pkgs.tree}/bin/tree --fromfile
       '';
     in
     {
