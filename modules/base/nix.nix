54e940500100649742d636638bbebf5a0e7bcd0a
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2025-10-10 12:58:33 +0800

    specify custom nix package version

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -17,6 +17,8 @@
         channel.enable = lib.mkDefault false;
         settings.nix-path = [ "nixpkgs=${pkgs.path}" ];
 
+        package = pkgs.nixVersions.nix_2_28;
+
         # From https://jackson.dev/post/nix-reasonable-defaults/
         extraOptions = ''
           connect-timeout = 5
