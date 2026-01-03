213a32a07f928f37392787852803a922c3490ccb
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2026-01-03 11:36:41 +0100

    add external monitor mirroring to hyprland config

---
+++ modules/hyprland/hyprland/monitor.nix
@@ -0,0 +1,19 @@
+{
+  flake.modules.homeManager.hyprland =
+    {
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      wayland.windowManager.hyprland.settings = {
+        monitor = [
+            # Internal panel (primary)
+            "eDP-1,2880x1800@60.00100,0x0,2"
+
+            #External monitors mirrored to eDP-1
+            "HDMI-A-1,preferred,auto,2,mirror,eDP-1"
+        ];
+      };
+    };
+}
