d6600a760a04ff5db10dcac7438826a9fcc865c7
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2026-01-19 14:07:00 +0100

    update layer rules and window rules to be compatible with hyprland v0.53.0

--- modules/hyprland/chatgpt.nix
+++ modules/hyprland/chatgpt.nix
@@ -1,24 +1,19 @@
 {
-  flake.modules.homeManager.hyprland =
-    { pkgs, ... }:
-    let
-      specialTitle = "chatgpt-special-terminal";
-      size = 70;
-      offset = (100 - size) / 2;
-    in
-    {
-      home.packages = [ pkgs.chatgpt-cli ];
-      wayland.windowManager.hyprland.settings = {
-        exec-once = [
-          "[workspace special:chatgpt silent; float; move ${toString offset}% ${toString offset}%; size ${toString size}% ${toString size}%] $terminal -T ${specialTitle} --hold sh -c 'while true; do chatgpt; clear; done'"
-        ];
-        windowrule = [ "workspace special:chatgpt, title:${specialTitle}" ];
-        bind = [
-          # mapping chatgpt to the windows assistant key
-          "SUPER SHIFT, code:201, togglespecialworkspace, chatgpt"
-
-        ];
-      };
-
+  flake.modules.homeManager.hyprland = {pkgs, ...}: let
+    specialTitle = "chatgpt-special-terminal";
+    size = 70;
+    offset = (100 - size) / 2;
+  in {
+    home.packages = [pkgs.chatgpt-cli];
+    wayland.windowManager.hyprland.settings = {
+      exec-once = [
+        "[workspace special:chatgpt silent; float; move ${toString offset}% ${toString offset}%; size ${toString size}% ${toString size}%] $terminal -T ${specialTitle} --hold sh -c 'while true; do chatgpt; clear; done'"
+      ];
+      windowrule = ["workspace special:chatgpt, match:title ${specialTitle}"];
+      bind = [
+        # mapping chatgpt to the windows assistant key
+        "SUPER SHIFT, code:201, togglespecialworkspace, chatgpt"
+      ];
     };
+  };
 }
--- modules/hyprland/hyprland/windows.nix
+++ modules/hyprland/hyprland/windows.nix
@@ -1,48 +1,44 @@
 {
-  flake.modules.homeManager.hyprland =
-    {
-      ...
-    }:
-    {
-      wayland.windowManager.hyprland.settings = {
-        windowrule = [
-          # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
-          "suppressevent maximize, class:.*"
+  flake.modules.homeManager.hyprland = {...}: {
+    wayland.windowManager.hyprland.settings = {
+      windowrule = [
+        # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
+        "suppress_event maximize, match:class .*"
 
-          # Force chromium into a tile to deal with --app bug
-          "tile, class:^(chromium)$"
+        # Force chromium into a tile to deal with --app bug
+        "tile on, match:class ^(chromium)$"
 
-          # Settings management
-          "float, class:^(org.pulseaudio.pavucontrol|.blueman-manager-wrapped)$"
+        # Settings management
+        "float on, match:class ^(org.pulseaudio.pavucontrol|.blueman-manager-wrapped)$"
 
-          # Float Steam, fullscreen RetroArch
-          "float, class:^(steam)$"
-          "fullscreen, class:^(com.libretro.RetroArch)$"
+        # Float Steam, fullscreen RetroArch
+        "float on, match:class ^(steam)$"
+        "fullscreen on, match:class ^(com.libretro.RetroArch)$"
 
-          # Just dash of transparency
-          "opacity 0.97 0.9, class:.*"
-          # Normal chrome Youtube tabs
-          "opacity 1 1, class:^(chromium|google-chrome|google-chrome-unstable)$, title:.*Youtube.*"
-          "opacity 1 0.97, class:^(chromium|google-chrome|google-chrome-unstable)$"
-          "opacity 0.97 0.9, initialClass:^(chrome-.*-Default)$ # web apps"
-          "opacity 1 1, initialClass:^(chrome-youtube.*-Default)$ # Youtube"
-          "opacity 1 1, class:^(zoom|vlc|org.kde.kdenlive|com.obsproject.Studio)$"
-          "opacity 1 1, class:^(com.libretro.RetroArch|steam)$"
+        # Just dash of transparency
+        "opacity 0.97 0.9, match:class .*"
+        # Normal chrome Youtube tabs
+        "opacity 1 1, match:class ^(chromium|google-chrome|google-chrome-unstable)$, match:title .*Youtube.*"
+        "opacity 1 0.97, match:class ^(chromium|google-chrome|google-chrome-unstable)"
+        "opacity 0.97 0.9, match:initial_class ^(chrome-.*-Default)$ # web apps"
+        "opacity 1 1, match:initial_class ^(chrome-youtube.*-Default)$ # Youtube"
+        "opacity 1 1, match:class ^(zoom|vlc|org.kde.kdenlive|com.obsproject.Studio)$"
+        "opacity 1 1, match:class ^(com.libretro.RetroArch|steam)$"
 
-          # Fix some dragging issues with XWayland
-          "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+        # Fix some dragging issues with XWayland
+        "no_focus on, match:class ^$, match:title ^$, match:xwayland 1, match:float 1, match:fullscreen 0, match:pin 0"
 
-          # Float in the middle for clipse clipboard manager
-          "float, class:(clipse)"
-          "size 622 652, class:(clipse)"
-          "stayfocused, class:(clipse)"
-        ];
+        # Float in the middle for clipse clipboard manager
+        "float on, match:class (clipse)"
+        "size 622 652, match:class (clipse)"
+        "stay_focused on, match:class (clipse)"
+      ];
 
-        layerrule = [
-          # Proper background blur for wofi
-          "blur,wofi"
-          "blur,waybar"
-        ];
-      };
+      layerrule = [
+        # Proper background blur for wofi
+        "blur on, match:namespace wofi"
+        "blur on, match:namespace waybar"
+      ];
     };
+  };
 }
