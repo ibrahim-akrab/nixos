fb03c7d075e4a46bc187e1b785d4783ee88480e4
Ibrahim Akrab <ibrahim.m.akrab@gmail.com>
2026-02-17 20:20:37 +0100

    remove hyprland cachix from extra-trusted-substituters

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -23,7 +23,6 @@
         };
         nix.settings = {
           substituters = [ "https://hyprland.cachix.org" ];
-          extra-trusted-substituters = [ "https://hyprland.cachix.org" ];
           trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
         };
       };
